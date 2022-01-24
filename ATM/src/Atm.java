01602e047b2b709e691b4e6f4b29d26a7500dd73
Ivailo Draganov <ivaillo.draganov@gmail.com>
2022-01-24 17:53:46 +0200

    Update Atm file

--- ATM/src/Atm.java
+++ ATM/src/Atm.java
@@ -1,2 +1,8 @@
 public class Atm {
+    public static void main(String[] args) {
+        // I have put the url,username and password into the args.
+        BankService bankService = new BankService(args[0],args[1],args[2]);
+        MenuService menu = new MenuService(bankService);
+        menu.start();
+    }
 }
